a1fc8e6929d3f16f89e6d474b7ff88ccf365b756
Gideon888-Inter <accounts@interlandsa.co.za>
2026-06-18 10:22:46 +0200

    Update Login.jsx

--- logisticspro/frontend/src/pages/Login.jsx
+++ logisticspro/frontend/src/pages/Login.jsx
@@ -34,6 +34,7 @@ export default function Login() {
   return (
     <div style={{
       minHeight: '100vh',
+      height: '100vh',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
